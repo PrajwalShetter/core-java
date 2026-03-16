211a78337ce7ab1ae3a2abb74faa22c5fe0c59d2
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-03-16 14:40:23 +0530

    Deleted successfully

--- ClassDeclaration/Friends.java
+++
@@ -1,9 +0,0 @@
-class Friends{
-	public static void main(String[] args){
-		System.out.println("Bhargav");
-		System.out.println("Chiranjeevi");
-		System.out.println("Vinayak");
-		System.out.println("Jeevan");
-		System.out.println("Sundar");
-}
-}
