a8e0a05dc547864e32262083ada942dba78a2fd0
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-03-26 23:22:07 +0530

    Day -12 Instance variables and object creation programs

---
+++ instance/IpadRunner.java
@@ -0,0 +1,80 @@
+class IpadRunner {
+
+	public static void main(String[] args){
+
+		Ipad i1 = new Ipad();
+
+		System.out.println("Default Values: ");
+		System.out.println("Model = "+ i1.model + " \n Color = " + i1.color + " \n Price = " + i1.price + " \n PencilSupport = " + i1.hasPencilSupport + " \n Storage = " + i1.storage+"\n");
+
+		i1.model="iPad 9th Gen";
+		i1.color="Space Gray";
+		i1.price=30000;
+		i1.hasPencilSupport=true;
+		i1.storage=64;
+
+		System.out.println("Updated Values:");
+		System.out.println("Model = "+i1.model + " \n Color = " + i1.color + " \n Price =" + i1.price + "\n PencilSupport= " + i1.hasPencilSupport + "\n Storage = " + i1.storage+"\n");
+
+
+		Ipad i2 = new Ipad();
+
+		System.out.println("Default Values: ");
+		System.out.println("Model = "+ i2.model + " \n Color = " + i2.color + " \n Price = " + i2.price + " \n PencilSupport = " + i2.hasPencilSupport + " \n Storage = " + i2.storage+"\n");
+
+		i2.model="iPad Air";
+		i2.color="Blue";
+		i2.price=55000;
+		i2.hasPencilSupport=true;
+		i2.storage=128;
+
+		System.out.println("Updated Values:");
+		System.out.println("Model = "+i2.model + " \n Color = " + i2.color + " \n Price =" + i2.price + "\n PencilSupport= " + i2.hasPencilSupport + "\n Storage = " + i2.storage+"\n");
+
+
+		Ipad i3 = new Ipad();
+
+		System.out.println("Default Values: ");
+		System.out.println("Model = "+ i3.model + " \n Color = " + i3.color + " \n Price = " + i3.price + " \n PencilSupport = " + i3.hasPencilSupport + " \n Storage = " + i3.storage+"\n");
+
+		i3.model="iPad Pro";
+		i3.color="Silver";
+		i3.price=80000;
+		i3.hasPencilSupport=true;
+		i3.storage=256;
+
+		System.out.println("Updated Values:");
+		System.out.println("Model = "+i3.model + " \n Color = " + i3.color + " \n Price =" + i3.price + "\n PencilSupport= " + i3.hasPencilSupport + "\n Storage = " + i3.storage+"\n");
+
+
+		Ipad i4 = new Ipad();
+
+		System.out.println("Default Values: ");
+		System.out.println("Model = "+ i4.model + " \n Color = " + i4.color + " \n Price = " + i4.price + " \n PencilSupport = " + i4.hasPencilSupport + " \n Storage = " + i4.storage+"\n");
+
+		i4.model="iPad Mini";
+		i4.color="Purple";
+		i4.price=50000;
+		i4.hasPencilSupport=true;
+		i4.storage=128;
+
+		System.out.println("Updated Values:");
+		System.out.println("Model = "+i4.model + " \n Color = " + i4.color + " \n Price =" + i4.price + "\n PencilSupport= " + i4.hasPencilSupport + "\n Storage = " + i4.storage+"\n");
+
+
+		Ipad i5 = new Ipad();
+
+		System.out.println("Default Values: ");
+		System.out.println("Model = "+ i5.model + " \n Color = " + i5.color + " \n Price = " + i5.price + " \n PencilSupport = " + i5.hasPencilSupport + " \n Storage = " + i5.storage+"\n");
+
+		i5.model="iPad 10th Gen";
+		i5.color="Yellow";
+		i5.price=45000;
+		i5.hasPencilSupport=true;
+		i5.storage=64;
+
+		System.out.println("Updated Values:");
+		System.out.println("Model = "+i5.model + " \n Color = " + i5.color + " \n Price =" + i5.price + "\n PencilSupport= " + i5.hasPencilSupport + "\n Storage = " + i5.storage+"\n");
+
+	}
+}
