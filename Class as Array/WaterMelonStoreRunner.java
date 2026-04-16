2c9d96eb782012154d357fa2d42082cf0ac63b7d
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-16 23:59:41 +0530

    Day 24, 25 implementing store,dispaly, serach and update method using class as array

--- Class as Array/WaterMelonStoreRunner.java
+++ Class as Array/WaterMelonStoreRunner.java
@@ -33,6 +33,14 @@ class WaterMelonStoreRunner{
 		}
 		
 		store.display();
-		
+		System.out.println("");
+		store.searchByLocation("mysore");
+		System.out.println("");
+		WaterMelon ref = store.findByLocation("koappal");
+		if(ref != null){
+			ref.display();
+		}
+		System.out.println("");
+		store.updatePriceByLocation(90.00,"vijayapura");
 	}
 }
