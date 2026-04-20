fdcfada6a7f78e6faeade4a7cc075a97a7bff99b
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-20 23:32:03 +0530

    Day 27 Inheritence practice program

---
+++ Inheritence/Camera.java
@@ -0,0 +1,17 @@
+class Camera{
+	 
+	 String brand;
+	 double price;
+	 
+	 Camera(String brand, double price){
+		 this.brand = brand;
+		 this.price = price;
+	 }
+	 
+	 void display(){
+		System.out.println("brand : " + this.brand);
+		System.out.println("price : " + this.price);
+	}
+ }
+ 
+ 
