6fbfc9f258400fa3feb50c593b68b0c3b58369cf
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-21 23:22:04 +0530

    Implemented constructor chaining using this keyword

---
+++ Inheritence/chaining/CoinRunner.java
@@ -0,0 +1,11 @@
+
+class CoinRunner{
+	
+	public static void main(String[] args){
+	Coin coin1 = new Coin("India","steel",1, 2012,"Circle");
+	Coin coin2 = new Coin("USA","steel",1, 2018,"pentagon");
+	
+	coin1.display();
+	coin2.display();
+	}
+}
