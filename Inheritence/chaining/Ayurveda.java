6952a0fb4cb8001a7979171360d7837220319636
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-21 23:29:27 +0530

    Implemented constructor chaining using super keyword

---
+++ Inheritence/chaining/Ayurveda.java
@@ -0,0 +1,6 @@
+class Ayurveda extends Hospital{
+	
+	Ayurveda(String name, String location, int noOfBeds, boolean emergencyAvailable, String type){
+		super(name, location, noOfBeds, emergencyAvailable, type);
+	}
+}
---
+++ Inheritence/chaining/HospitalRunner.java
@@ -0,0 +1,13 @@
+
+class HospitalRunner{
+	
+	public static void main(String[] args){
+	Ayurveda ayurveda1 = new Ayurveda("dhanvantri","Bang", 12, false, "Ayurveda");
+	Ayurveda ayurveda2 = new Ayurveda("hugingface", "gulbarga", 0, false, "Ayurveda");
+	
+	ayurveda1.display();
+	ayurveda2.display();
+	}
+}
+
+
