3a66faa0926200766cc4162fb3546a2dbec95a2e
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-21 23:35:07 +0530

    Demonstrated usage of final keyword for class restriction

---
+++ Inheritence/chaining/RestaurantRunner.java
@@ -0,0 +1,10 @@
+
+class RestaurantRunner{
+	
+	public static void main(String[] args){
+		
+		VegRestaurant veg1 = new VegRestaurant("Udupi", "Bangalore", 5);
+		veg1.display();
+	}
+}
+
---
+++ Inheritence/chaining/VegRestaurant.java
@@ -0,0 +1,6 @@
+class VegRestaurant extends Restaurant{
+	
+	VegRestaurant(String name, String location, int rating){
+		super(name, location, rating);
+	}
+}
