55eae6c332a5978401617cbafd2e63385cdc0b25
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-22 19:12:51 +0530

    Day 29 Inheritence practice program

---
+++ Inheritence/TouchPad.java
@@ -0,0 +1,17 @@
+class TouchPad{
+	 
+	 String type;
+	 double size;
+	 
+	 TouchPad(String type, double size){
+		 this.type = type;
+		 this.size = size;
+	 }
+	 
+	 void display(){
+		System.out.println("type : " + this.type);
+		System.out.println("size : " + this.size);
+	}
+ }
+ 
+ 
