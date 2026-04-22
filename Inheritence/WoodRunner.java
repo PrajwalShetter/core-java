5d50d1851bf3aed09d8ae23e5ba85a1b10abfc75
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-22 19:20:00 +0530

    Day 29 Inheritence practice program

---
+++ Inheritence/WoodRunner.java
@@ -0,0 +1,11 @@
+
+class WoodRunner{
+	
+	public static void main(String[] args){
+	FurnitureWood wood1 = new FurnitureWood("Teak",2000);
+	FurnitureWood wood2 = new FurnitureWood("Oak",3000);
+		
+		wood1.display();
+		wood2.display();
+	}
+}
