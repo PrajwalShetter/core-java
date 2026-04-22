6661977c81419d34947f58be230913da9fe30262
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-22 19:16:53 +0530

    Day 29 Inheritence practice program

---
+++ Inheritence/KnifeRunner.java
@@ -0,0 +1,13 @@
+
+ class KnifeRunner{
+	 
+	 public static void main(String[] args){
+		 
+	 Knife knife1= new Knife("steel", 5.9);
+	 Knife knife2 = new Knife("Iron", 8.0);
+	 
+	 knife1.display();
+	 knife2.display();
+	 
+	 }
+ }
