801459de2e75b0ded13374c70cc8635e68571224
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-22 19:14:53 +0530

    Day 29 Inheritence practice program

---
+++ Inheritence/DumbBellRunner.java
@@ -0,0 +1,12 @@
+
+class DumbBellRunner{
+	
+	public static void main(String[] args){
+		
+	GymDumbBell dumbell1 = new GymDumbBell(10.5,"Iron");
+	GymDumbBell dumbell2 = new GymDumbBell(15.0,"Steel");
+		
+		dumbell1.display();
+		dumbell2.display();
+	}
+}
