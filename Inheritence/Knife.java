7a9a6f2a35239e137173161e6b83d6cb79819f72
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-22 19:17:09 +0530

    Day 29 Inheritence practice program

---
+++ Inheritence/Knife.java
@@ -0,0 +1,16 @@
+class Knife{
+	 
+	 String type;
+	 double length;
+	 
+	 Knife(String type, double length){
+		 this.type = type;
+		 this.length = length;
+	 }
+	 
+	 void display(){
+		System.out.println("type : " + this.type);
+		System.out.println("length : " + this.length);
+	}
+ }
+ 
