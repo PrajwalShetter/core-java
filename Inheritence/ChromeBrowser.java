ad666d9d42eae178d72bc0b4406a4eeae1e6c522
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-23 23:45:03 +0530

    day 30 understanding run time polymorphisum

---
+++ Inheritence/ChromeBrowser.java
@@ -0,0 +1,11 @@
+class ChromeBrowser extends Browser{
+	
+	void popular(){
+		System.out.println("Running in ChromeBrowser");
+	}
+	
+	@Override
+	void search(){
+		System.out.println("Running search in ChromeBrowser");
+	}
+}
