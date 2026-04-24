a00da444980adea675e790445b9b9b975f78de36
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-24 22:05:00 +0530

    day 30 understanding run time polymorphisum

--- Inheritence/BrowserRunner.java
+++ Inheritence/BrowserRunner.java
@@ -2,7 +2,8 @@ class BrowserRunner{
 	
 	public static void main(String[] args){
 		
-	Browser query = new FireFoxBrowser();
+	Browser query = new ChromeBrowser();
+	
 	
 	Surf app = new Surf();
 	app.browser(query);
