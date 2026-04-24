e5d30336636e8c4ce7ec6a6410621b74cc1689e6
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-04-24 22:05:34 +0530

    day 30 understanding run time polymorphisum

--- Inheritence/Surf.java
+++ Inheritence/Surf.java
@@ -6,6 +6,9 @@ class Surf{
 		
 		if(browser instanceof ChromeBrowser){
 			System.out.println("Browser is a chromebrowser");
+			
+			ChromeBrowser cb = (ChromeBrowser) browser; 
+			cb.popular();
 		}
 		else if(browser instanceof FireFoxBrowser){
 			System.out.println("browser is a FireFoxBrowser");
