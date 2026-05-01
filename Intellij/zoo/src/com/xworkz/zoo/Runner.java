2d7a4e5c1f389e56c06acf3ab4f323ed06f0c79c
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-01 23:36:17 +0530

    Day-31,32 Encapsulation practice programme

--- Intellij/zoo/src/com/xworkz/zoo/Runner.java
+++ Intellij/zoo/src/com/xworkz/zoo/Runner.java
@@ -1,7 +1,15 @@
 package com.xworkz.zoo;
+import com.xworkz.zoo.management.TicketManagement;
+import com.xworkz.zoo.management.VehicleTickets;
+import com.xworkz.zoo.management.WalkTickets;
+import com.xworkz.zoo.service.GuideService;
 
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi iam Prajwal");
+
+        TicketManagement ticketManagement = new VehicleTickets();
+        ticketManagement.paperQuality();
+
     }
 }
