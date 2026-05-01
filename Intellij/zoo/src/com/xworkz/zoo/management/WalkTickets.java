7d8ac7cf18105c14bea8ce9de86d07011d88cfa4
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-01 23:40:25 +0530

    Day-31,32 Encapsulation practice programme

--- Intellij/zoo/src/com/xworkz/zoo/management/WalkTickets.java
+++ Intellij/zoo/src/com/xworkz/zoo/management/WalkTickets.java
@@ -1,4 +1,9 @@
 package com.xworkz.zoo.management;
 
-public class WalkTickets {
+public class WalkTickets extends TicketManagement{
+
+////    @Override
+//    public void paperQuality1(){
+//        System.out.println("Running from WalkTickets");
+//    }
 }
