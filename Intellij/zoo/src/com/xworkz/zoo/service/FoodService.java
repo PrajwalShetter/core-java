55d88b8f10a804e4fde1f257e12830a44d7fab9f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-01 23:41:45 +0530

    Day-31,32 Encapsulation practice programme

--- Intellij/zoo/src/com/xworkz/zoo/service/FoodService.java
+++ Intellij/zoo/src/com/xworkz/zoo/service/FoodService.java
@@ -1,4 +1,11 @@
 package com.xworkz.zoo.service;
+import com.xworkz.zoo.management.TicketManagement;
 
-public class FoodService {
+public class FoodService extends TicketManagement {
+
+
+//    @Override
+//   public void paperQuality(){
+//        System.out.println("Running from Foodservice");
+//    }
 }
