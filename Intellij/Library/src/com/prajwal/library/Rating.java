a0bf21ce0178cfac3824cb1c160abb909eb93081
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-12 23:52:15 +0530

    Day 33 getters and setters invoke and understanding

---
+++ Intellij/Library/src/com/prajwal/library/Rating.java
@@ -0,0 +1,35 @@
+package com.prajwal.library;
+
+public class Rating {
+    private String username;
+    private int stars;
+    private String review;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String toString(){
+        return "I am rating the library";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public String getReview() {
+        return review;
+    }
+}
