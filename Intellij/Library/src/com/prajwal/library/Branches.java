147b27699bde747e9e702f3fd6fded22d78e8acb
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-12 23:24:57 +0530

    Day 33 getters and setters invoke and understanding

---
+++ Intellij/Library/src/com/prajwal/library/Branches.java
@@ -0,0 +1,42 @@
+package com.prajwal.library;
+
+public class Branches {
+    private  String location;
+    private String name;
+    private double sqft;
+    private boolean govt;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGovt(boolean govt) {
+        this.govt = govt;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setSqft(double sqft) {
+        this.sqft = sqft;
+    }
+
+    public String toString(){
+        return "prajwal 1234";
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public boolean getGovt(){
+        return this.govt;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public double getSqft() {
+        return sqft;
+    }
+}
