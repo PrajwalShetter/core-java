1ad8416d103308013710ee30bede4ac26a3e6c00
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-12 23:25:16 +0530

    Day 33 getters and setters invoke and understanding

---
+++ Intellij/Library/src/com/prajwal/library/Inogration.java
@@ -0,0 +1,44 @@
+package com.prajwal.library;
+
+public class Inogration {
+    private String date;
+    private int activities;
+    private String chifeGuest;
+    private int fond;
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setActivities(int activities) {
+        this.activities = activities;
+    }
+
+    public void setChifeGuest(String chifeGuest) {
+        this.chifeGuest = chifeGuest;
+    }
+
+    public void setFound(int fond) {
+        this.fond = fond;
+    }
+
+    public String toString(){
+        return "Hi i am prajwal";
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getActivities() {
+        return activities;
+    }
+
+    public String getChifeGuest() {
+        return chifeGuest;
+    }
+
+    public int getFond() {
+        return fond;
+    }
+}
