1504976a924ae43886627bf2412c0dde2cced549
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-20 23:23:51 +0530

    Day 35 Practicing CRUD Operations on University relationship based problem

---
+++ Class as Array/University CRUD_Operation/Principal.java
@@ -0,0 +1,19 @@
+class Principal{
+	
+	String name;
+	String qualification;
+	int experience;
+	
+	void Principal(String name, String qualification, int experience){
+		this.name= name;
+		this.qualification = qualification;
+		this.experience = experience;
+		
+	}
+	
+	void display(){
+		System.out.println("Name :"+this.name);
+		System.out.println("Experience :"+this.experience);
+		System.out.println("Qualification :"+this.qualification);
+	}
+}
