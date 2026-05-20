76782c759e2f87a4949e169481b6ef9f45b1c140
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-20 23:26:38 +0530

    Day 35 Practicing CRUD Operations on University relationship based problem

---
+++ Class as Array/University CRUD_Operation/University.java
@@ -0,0 +1,80 @@
+// Declare below classes
+
+// University:
+// name, viceChancellor, College[]
+
+// ViceChancellor:
+// name, experience, Address
+
+// College:
+// name, principal, Department[], noOfStudents
+
+// Principal:
+// name, qualification, experience
+
+// Department:
+// name, HOD, Student[], noOfFaculty
+
+// HOD:
+// name, specialization, yearsOfExperience
+
+// Student:
+// name, rollNo, marks, Address
+
+// Address:
+// doorNo, area, pincode, city
+
+
+// UniversityStore:
+// University[10]
+
+// save(University university)
+
+// University findByUniversityName(String name)
+
+// College findCollegeByName(String name)
+
+// Department[] findAllDepartmentsByCollegeName(String collegeName)
+
+// Student findStudentByRollNo(int rollNo)
+
+// int findNoOfStudentsByDepartmentName(String deptName)
+
+// University findByPrincipalName(String principalName)
+
+// Student[] findStudentsByMarksGreaterThan(int marks)
+
+// College findCollegeByHODName(String hodName)
+
+// String findViceChancellorNameByStudentRollNo(int rollNo)
+
+class University{
+	
+	String name;
+	ViceChancellor viceChancellor;
+	College college[];
+	
+	void University(String name, VoiceChancellor voiceChancellor, College college[]){
+		this.name=name;
+		this.voiceChancellor = voiceChancellor;
+		this.college = college;
+	}
+	
+	void display(){
+		System.out.println("--------------");
+		System.out.println("Name :"+this.name);
+		System.out.println("ViceChancellor :"+this.viceChancellor.name);
+		
+		if(this.college!=null){
+			System.out.println("Colleges are :");
+			for(College clg: college){
+				if(clg != null){
+					System.out.println("Name :"+clg.name);
+				}
+			
+		}
+		}
+		
+		
+	}
+}
