b53ad6b4fd85b5dcf3b9e61fc4279b5d75a070f7
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-20 23:23:25 +0530

    Day 35 Practicing CRUD Operations on University relationship based problem

---
+++ Class as Array/University CRUD_Operation/College.java
@@ -0,0 +1,29 @@
+class College{
+	
+	String name;
+	Principal principal;
+	Department department[];
+	int noOfStudents;
+	
+	void College(String name, Principal principal, Department[] department, int noOfStudents){
+		this.name= name;
+		this.principal=principal;
+		this.department=department;
+		this.noOfStudents=noOfStudents;
+	}
+	
+	void display(){
+		System.out.println("Name :"+this.name);
+		System.out.println("Principal :"+this.principal.name);
+		System.out.println("NoOfStudents :"+this.noOfStudents);
+		
+		if(department != null){
+			System.out.println("departments are :");
+			for(Department dpt: department){
+				if(dpt != null){
+					System.out.println("Name :"+dpt.name);
+				}
+			}
+		}
+	}
+}
