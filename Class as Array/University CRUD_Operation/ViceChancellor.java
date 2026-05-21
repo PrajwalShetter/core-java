af55e935515928769405311e5b2c94b6735f6356
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-21 22:14:43 +0530

    Day 35&36 Practicing CRUD Operations on University relationship based problem

--- Class as Array/University CRUD_Operation/ViceChancellor.java
+++ Class as Array/University CRUD_Operation/ViceChancellor.java
@@ -4,15 +4,15 @@ class ViceChancellor{
 	int experience;
 	Address address;	
 	
-	void ViceChancellor(String name, int experience, Address address ){
+	 ViceChancellor(String name, int experience, Address address){
 		this.name= name;
 		this.experience = experience;
 		this.address = address;
 	}
 	
 	void display(){
-		System.out.println("Name :"this.name);
-		System.out.println("Experience :"this.experience);
+		System.out.println("Name :"+this.name);
+		System.out.println("Experience :"+this.experience);
 		System.out.println("Address :"+this.address.area);
 		
 	}
