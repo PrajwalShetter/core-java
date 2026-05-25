5963ced6ed8704127e3aa532fbc1be49e9afc19d
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-25 22:20:30 +0530

    Aptitude programs

---
+++ Aptitude/Vowels.java
@@ -0,0 +1,19 @@
+public class Vowels {
+    public static void main(String[] args) {
+
+        String s="Prajwal";
+        int count=0;
+
+        for(int i=0;i<s.length();i++){
+
+            char ch=
+            Character.toLowerCase(s.charAt(i));
+
+            if(ch=='a'||ch=='e'||ch=='i'
+                    ||ch=='o'||ch=='u')
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
