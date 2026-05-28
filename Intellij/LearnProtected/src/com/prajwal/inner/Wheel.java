d71e224cc1c406c0b14996399edefcd0700556dd
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-28 22:42:47 +0530

    day 39 Learning protected keyword

---
+++ Intellij/LearnProtected/src/com/prajwal/inner/Wheel.java
@@ -0,0 +1,23 @@
+package com.prajwal.inner;
+
+public class Wheel {
+
+    public static void steelWheel(){
+        System.out.println("I am steelWheel");
+    }
+
+    private void woodWheel(){
+        System.out.println("i am woodWheel");
+    }
+
+    void waterWheel(){
+        System.out.println("I am waterWheel");
+    }
+
+    protected void rubberWheel(){
+        System.out.println("i am RubberWheel");
+    }
+
+
+
+}
