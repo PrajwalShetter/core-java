fcba4454af04befb04d41d8843374af11932a015
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-28 22:42:05 +0530

    day 39 Learning protected keyword

---
+++ Intellij/LearnProtected/src/com/prajwal/inner/Prajwal.java
@@ -0,0 +1,15 @@
+package com.prajwal.inner;
+
+public class Prajwal extends Pizza {
+
+
+    public static void main(String[] args) {
+
+         Pizza pizza = new Pizza();
+         Wheel wheel = new Wheel();
+
+         Wheel.steelWheel();
+        wheel.waterWheel();
+
+    }
+}
