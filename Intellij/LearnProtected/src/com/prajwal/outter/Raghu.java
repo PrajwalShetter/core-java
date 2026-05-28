6972347ac8b7f971876a519086c519540acc27f5
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-28 23:02:26 +0530

    Day-39 learning protected key word

--- Intellij/LearnProtected/src/com/prajwal/outter/Raghu.java
+++ Intellij/LearnProtected/src/com/prajwal/outter/Raghu.java
@@ -3,14 +3,14 @@ package com.prajwal.outter;
 import com.prajwal.inner.Pizza;
 import com.prajwal.inner.Wheel;
 
-public class Raghu extends Wheel {
+public class Raghu extends Pizza {
     public Raghu() {
         super();
     }
 
     public static void main(String[] args) {
-//        Pizza pizza = new Pizza();
-
+//  Pizza pizza = new Pizza();
+        Raghu raghu = new Raghu();
 
     }
 }
