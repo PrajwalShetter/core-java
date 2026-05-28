b99ccdf6875fd41db9b3c760666feb6b4ff7acf5
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-05-28 22:41:37 +0530

    day 39 Learning protected keyword

---
+++ Intellij/LearnProtected/src/com/prajwal/inner/Pizza.java
@@ -0,0 +1,27 @@
+package com.prajwal.inner;
+
+public class Pizza {
+
+    private String pizzaName;
+    private double cost;
+    private String size;
+
+    // public:
+    // Accessible anywhere in the project
+    // (inside package and outside package)
+
+    // default:
+    // Accessible only within the same package
+
+    // protected:
+    // Accessible within the same package
+    // and outside package through inheritance
+
+    // private:
+    // Accessible only inside the same class
+
+    public Pizza() {
+
+    }
+}
+
