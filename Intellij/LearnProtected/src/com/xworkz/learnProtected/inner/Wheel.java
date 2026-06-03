634f404d804622bc7f5f43bcf420806d983eccdd
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-03 23:19:06 +0530

    Day-39&40 Learning protected accesses specifire in encapsulation concept

---
+++ Intellij/LearnProtected/src/com/xworkz/learnProtected/inner/Wheel.java
@@ -0,0 +1,23 @@
+package com.xworkz.learnProtected.inner;
+
+public class Wheel {
+
+    public static void steelWheel(){
+        System.out.println("I am steelWheel");
+    }
+
+    private void woodWheel(){
+        System.out.println("i am woodWheel");
+    }
+
+    void waterWheel(){
+        System.out.println("I am waterWheel");
+    }
+
+    protected void rubberWheel(){
+        System.out.println("i am RubberWheel");
+    }
+
+
+
+}
