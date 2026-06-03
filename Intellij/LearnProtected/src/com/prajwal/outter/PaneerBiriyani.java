2656e1f9658d27d284a014aa73253c5fb98d8ae1
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-03 08:56:36 +0530

    Learning protected access specifier

---
+++ Intellij/LearnProtected/src/com/prajwal/outter/PaneerBiriyani.java
@@ -0,0 +1,25 @@
+package com.prajwal.outter;
+
+import com.prajwal.inner.Food;
+
+public class PaneerBiriyani extends Food {
+
+    public void test(){
+        System.out.println(spices);
+        process(); // here invoking protected method in different package through inheritence
+    }
+
+    public void test1() {
+        Food f = new Food();
+        f.process();
+        System.out.println(f.spices);
+        //PaneerBiriyani is a subclass, it cannot access protected members through a parent-class reference (Food f).
+    }
+
+    public static void main(String[] args) {
+        PaneerBiriyani paneerBiriyani = new PaneerBiriyani();
+        paneerBiriyani.process();
+        paneerBiriyani.test();
+    }
+
+}
