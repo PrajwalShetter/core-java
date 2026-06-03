436cb52a4a27068fc60759c832c8c76337ced2ab
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-03 20:52:19 +0530

    Day-39&40 Learning protected accesses specifire in encapsulation concept

---
+++ Intellij/LearnProtected/src/com/xworkz/learnProtected/inner/Pizza.java
@@ -0,0 +1,34 @@
+package com.xworkz.learnProtected.inner;
+
+public class Pizza {
+
+    private String pizzaName;
+    private double cost;
+    private String size;
+
+    // public:
+    // Accessible anywhere in the project
+    // (inside package and outside package)
+
+    // default:
+    // Accessible only within the same package
+
+    // protected:
+    // Accessible within the same package
+    // and outside package through inheritance
+
+    // private:
+    // Accessible only inside the same class
+
+    protected Pizza() {
+
+        ingriedent();
+    }
+
+    private void ingriedent(){
+        System.out.println("I am private method");
+
+    }
+
+
+}
