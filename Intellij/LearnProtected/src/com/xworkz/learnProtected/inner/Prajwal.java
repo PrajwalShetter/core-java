bd5ecca1fde902720126a735dbc95b1bab24ac56
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-03 23:16:40 +0530

    Day-39&40 Learning protected accesses specifire in encapsulation concept

---
+++ Intellij/LearnProtected/src/com/xworkz/learnProtected/inner/Prajwal.java
@@ -0,0 +1,15 @@
+package com.xworkz.learnProtected.inner;
+
+public class Prajwal extends Pizza {
+
+
+    public static void main(String[] args) {
+
+        Pizza pizza = new Pizza();
+        Wheel wheel = new Wheel();
+
+        Wheel.steelWheel();
+        wheel.waterWheel();
+
+    }
+}
