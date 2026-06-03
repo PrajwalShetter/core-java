18f401c0ae28bf6716538b85f26c1a59a4d485ad
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-03 23:23:41 +0530

    Day-39&40 Learning protected accesses specifire in encapsulation concept

---
+++ Intellij/LearnProtected/src/com/xworkz/learnProtected/outer/Raghu.java
@@ -0,0 +1,16 @@
+package com.xworkz.learnProtected.outer;
+
+
+import com.xworkz.learnProtected.inner.Pizza;
+
+public class Raghu extends Pizza {
+    public Raghu() {
+        super();
+    }
+
+    public static void main(String[] args) {
+//  Pizza pizza = new Pizza();
+        Raghu raghu = new Raghu();
+
+    }
+}
