3cb010ce794c8173f064a4d11bd5a25e89ac2a29
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-08 23:21:48 +0530

    Day-42 Practicde programms on interface and implemnts

---
+++ Intellij/LearnAbstraction/src/com/xworz/learnInterface/inner/BankAccount.java
@@ -0,0 +1,21 @@
+package com.xworz.learnInterface.inner;
+
+public interface BankAccount {
+
+    String bankName= "SBI";
+    double minBalance = 1000.00;
+
+    void deposit();
+
+    void withdraw();
+
+    void checkBalance();
+
+    default void accountStatus(){
+        System.out.println("Active");
+    }
+
+    static void bankRules(){
+        System.out.println("lot of rules are there to maintain");
+    }
+}
