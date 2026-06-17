107382cc55cab764465207662a0157def7b0965f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-17 23:40:21 +0530

    interface with extends keyword

---
+++ Intellij/LearnAbstraction/src/com/xworkz/learnInterface/inner/VehiclePark.java
@@ -0,0 +1,12 @@
+package com.xworkz.learnInterface.inner;
+
+//interfaces supports multiple inheritance -- like one interface can inherit the multiple interfaces
+public interface VehiclePark {
+
+    int minCharge();
+
+    void modeOfPayment();
+
+
+
+}
