cef935359cdfd8f351d47f98ee63bd3a7fded81f
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-18 23:24:34 +0530

    interface with extends keyword

---
+++ Intellij/LearnAbstraction/src/com/xworkz/learnInterface/inner/PowerDevice.java
@@ -0,0 +1,12 @@
+package com.xworkz.learnInterface.inner;
+
+public interface PowerDevice {
+
+    String powerSource ="Electricity";
+    int voltage = 20;
+
+    void switchOn();
+
+    void switchOff();
+
+}
