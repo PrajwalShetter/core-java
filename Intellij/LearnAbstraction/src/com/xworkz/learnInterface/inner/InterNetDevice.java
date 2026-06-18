9fb0313bda525841440c385bf408d44a588a84b6
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-18 23:25:27 +0530

    interface with extends keyword

---
+++ Intellij/LearnAbstraction/src/com/xworkz/learnInterface/inner/InterNetDevice.java
@@ -0,0 +1,13 @@
+package com.xworkz.learnInterface.inner;
+
+public interface InterNetDevice {
+
+    String network ="wifi";
+    double speed = 100.00;
+
+    void connectInternet();
+
+    void disConnectInternet();
+
+
+}
