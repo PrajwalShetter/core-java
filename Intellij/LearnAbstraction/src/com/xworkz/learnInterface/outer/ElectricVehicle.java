65d18b8f332928615bf463bb46d87418fca04914
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-18 23:26:44 +0530

    interface with extends keyword

--- Intellij/LearnAbstraction/src/com/xworkz/learnInterface/outer/ElectricVehicle.java
+++ Intellij/LearnAbstraction/src/com/xworkz/learnInterface/outer/ElectricVehicle.java
@@ -11,6 +11,46 @@ public class ElectricVehicle implements VehiclePark {
 
     @Override
     public void modeOfPayment(){
-        System.out.println("Online");
+        System.out.println("payment Mode : Online");
+    }
+
+    @Override
+    public void deposit() {
+
+    }
+
+    @Override
+    public void withdraw() {
+
+    }
+
+    @Override
+    public void checkBalance() {
+
+    }
+
+    @Override
+    public void accountStatus() {
+        VehiclePark.super.accountStatus();
+    }
+
+    @Override
+    public void addToCart() {
+
+    }
+
+    @Override
+    public void placeOrder() {
+
+    }
+
+    @Override
+    public void cancelOrder() {
+
+    }
+
+    @Override
+    public void OfferDetails() {
+        VehiclePark.super.OfferDetails();
     }
 }
