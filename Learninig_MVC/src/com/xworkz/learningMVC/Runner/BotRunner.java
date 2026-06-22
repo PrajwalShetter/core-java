fd9b32e27c3939e968b80ed2c91685f58f120975
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-22 23:59:03 +0530

    Demonstartion of Scanner Keyword

--- Learninig_MVC/src/com/xworkz/learningMVC/Runner/BotRunner.java
+++ Learninig_MVC/src/com/xworkz/learningMVC/Runner/BotRunner.java
@@ -1,4 +1,22 @@
 package com.xworkz.learningMVC.Runner;
 
+import com.xworkz.learningMVC.inner.Bot;
+
+import java.util.Scanner;
+
 public class BotRunner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("hi  i am your Bot");
+        System.out.println("what you want to search");
+
+
+        String order = sc.nextLine();
+
+        Bot bot = new Bot();
+
+        bot.orderDetails(order);
+
+    }
 }
