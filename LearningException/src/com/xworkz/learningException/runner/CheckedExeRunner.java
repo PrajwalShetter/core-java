4f700894aff5e88ed70193911ff71003214984ed
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-06-26 23:58:25 +0530

    usage of catch block in different formats like single , multiple, multi

--- LearningException/src/com/xworkz/learningException/runner/CheckedExeRunner.java
+++ LearningException/src/com/xworkz/learningException/runner/CheckedExeRunner.java
@@ -1,11 +1,10 @@
-package com.xworkz.learningException;
+package com.xworkz.learningException.runner;
 
 import com.xworkz.learningException.checkedexception.CheckedException;
 import com.xworkz.learningException.checkedexception.CheckedException1;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 
 public class CheckedExeRunner {
     public static void main(String[] args) {
@@ -14,37 +13,6 @@ public class CheckedExeRunner {
             exceptionDemo.withdraw(-1500.00);
         System.out.println("Thank-you for using  application");
 
-        //2nd day -- Inbuilt checked
-        CheckedException1 checkedExce = new CheckedException1();
-
-        //single catch
-        try{
-            checkedExce.readFile();
-        }catch (FileNotFoundException e){
-            System.err.println("FileNotFoundException  handled");
-        }
-
-        //multiple catch
-        try{
-            checkedExce.readFile();
-            checkedExce.readClass();
-        }
-        catch (FileNotFoundException e){
-            System.err.println("FileNotFoundException  handled");
-        }
-        catch (ClassNotFoundException e){
-        System.err.println("ClassNotFoundException  handled");
-        }
-
-        //Multi
-        try{
-            checkedExce.sleep();
-            checkedExce.writeFile();
-            checkedExce.readClass();
-        }
-        catch(IOException | ClassNotFoundException | InterruptedException exception){
-            System.out.println("Exception handled succesfully");
-        }
 
     }
 }
