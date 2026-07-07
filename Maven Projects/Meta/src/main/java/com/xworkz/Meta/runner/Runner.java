f95891f8b0be555769aa39ff87592f192af45b8e
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-07 23:57:55 +0530

    Act as a UI and helps us to send data from UI to DAO and retrive data from database

--- Maven Projects/Meta/src/main/java/com/xworkz/Meta/runner/Runner.java
+++ Maven Projects/Meta/src/main/java/com/xworkz/Meta/runner/Runner.java
@@ -3,19 +3,39 @@ package com.xworkz.Meta.runner;
 import com.xworkz.Meta.controller.SignUpController;
 import com.xworkz.Meta.dto.SignUpDto;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
 
-        SignUpDto user1 = new SignUpDto();
-        user1.setUserName("Prajwal");
-        user1.setAge(23);
-        user1.setMobileNo("9908765432");
-        user1.setPassword("praj@8769");
-        user1.setConfirmPassword("praj@8769");
-        user1.setNationality("Indian");
+        SignUpDto user = new SignUpDto();
 
-        SignUpController signUpController = new SignUpController();
-        signUpController.register(user1);
+        user.setUserName("Prajwal");
+        user.setAge(23);
+        user.setMobileNo("9908765432");
+        user.setPassword("Praj@8769");
+        user.setConfirmPassword("Praj@8769");
+        user.setNationality("Indian");
+
+        SignUpController controller = new SignUpController();
+
+        controller.register(user);
+
+        controller.getAllData();
+
+        List<SignUpDto> list = controller.retrieveData();
+
+        for (SignUpDto dto : list){
+
+            System.out.println(dto.getUserName());
+            System.out.println(dto.getAge());
+            System.out.println(dto.getMobileNo());
+            System.out.println(dto.getPassword());
+            System.out.println(dto.getNationality());
+
+            System.out.println("------------------");
+        }
+
     }
 }
