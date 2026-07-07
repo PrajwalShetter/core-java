5ee2f685036d861cf71672f8d8e3fd659f56136a
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-07 23:53:58 +0530

    Added the getmethod and retrive method

--- Maven Projects/Meta/src/main/java/com/xworkz/Meta/controller/SignUpController.java
+++ Maven Projects/Meta/src/main/java/com/xworkz/Meta/controller/SignUpController.java
@@ -4,13 +4,32 @@ import com.xworkz.Meta.dto.SignUpDto;
 import com.xworkz.Meta.services.Impl.SignUpServicesImpl;
 import com.xworkz.Meta.services.SignUpServices;
 
+import java.util.List;
+
 public class SignUpController {
 
     SignUpServices signUpServices = new SignUpServicesImpl();
 
-public void register(SignUpDto user){
+    public void register(SignUpDto user) {
+
+        System.out.println("Controller register() Invoked");
+
+        signUpServices.register(user);
+
+    }
 
 
-}
+    public void getAllData() {
+
+        System.out.println("Controller getAllData() Invoked");
+
+        signUpServices.getAllData();
+
+    }
+
+    public List<SignUpDto> retrieveData(){
+
+        return  signUpServices.retrieveData();
+    }
 
 }
