a1c3fddb79e541c43a98d691a7b8d93cea6cd7d7
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-07 23:56:37 +0530

    In this interface creating methods called register, getalldata, retrive

--- Maven Projects/Meta/src/main/java/com/xworkz/Meta/dao/SignUpDao.java
+++ Maven Projects/Meta/src/main/java/com/xworkz/Meta/dao/SignUpDao.java
@@ -2,8 +2,14 @@ package com.xworkz.Meta.dao;
 
 import com.xworkz.Meta.dto.SignUpDto;
 
+import java.util.List;
+
 public interface SignUpDao {
 
     void register(SignUpDto user);
+
+    void getAllData();
+
+     List<SignUpDto> retrieveData();
 
 }
