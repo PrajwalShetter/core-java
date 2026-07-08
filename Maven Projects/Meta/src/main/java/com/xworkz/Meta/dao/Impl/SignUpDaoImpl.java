2948fb2690897a9f3da1669167b0617a0d9fa782
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-08 23:58:31 +0530

    Added getMethod using parameters

--- Maven Projects/Meta/src/main/java/com/xworkz/Meta/dao/Impl/SignUpDaoImpl.java
+++ Maven Projects/Meta/src/main/java/com/xworkz/Meta/dao/Impl/SignUpDaoImpl.java
@@ -3,16 +3,17 @@ package com.xworkz.Meta.dao.Impl;
 import com.xworkz.Meta.dao.SignUpDao;
 import com.xworkz.Meta.dto.SignUpDto;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Class.forName;
 
 public class SignUpDaoImpl implements SignUpDao {
+
+    String url ="jdbc:mysql://localhost:3306/meta_db";
+    String user = "root";
+    String password = "Pajju#123";
 
     @Override
     public void register(SignUpDto user) {
@@ -134,5 +135,112 @@ public class SignUpDaoImpl implements SignUpDao {
 
     }
     return list;
+    }
+
+    @Override
+    public void getById(int id) {
+
+        System.out.println("getById in DAO");
+
+        Connection connection = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/meta_db",
+                    "root",
+                    "Pajju#123"
+            );
+
+            String sql = "SELECT * FROM signup WHERE id = ?";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                System.out.println("ID           : " + resultSet.getInt("id"));
+                System.out.println("Username     : " + resultSet.getString("username"));
+                System.out.println("Age          : " + resultSet.getInt("age"));
+                System.out.println("Mobile No    : " + resultSet.getString("mobile_no"));
+                System.out.println("Password     : " + resultSet.getString("password"));
+                System.out.println("Nationality  : " + resultSet.getString("nationality"));
+
+            } else {
+
+                System.out.println("No Record Found for ID : " + id);
+
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (connection != null) {
+                    connection.close();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void getByIdAndName(int id, String name) {
+
+        System.out.println("getByIdAndName in DAO");
+
+        Connection connection = null;
+
+        try{
+            forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/meta_db",
+                    "root",
+                    "Pajju#123"
+            );
+
+            String sql = "select * from signup where id=? and UserName= ?";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1,id);
+            statement.setString(2,name);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+
+                System.out.println("ID           : " + resultSet.getInt("id"));
+                System.out.println("Username     : " + resultSet.getString("username"));
+                System.out.println("Age          : " + resultSet.getInt("age"));
+                System.out.println("Mobile No    : " + resultSet.getString("mobile_no"));
+                System.out.println("Password     : " + resultSet.getString("password"));
+                System.out.println("Nationality  : " + resultSet.getString("nationality"));
+            }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }finally {
+            try{
+                if(connection != null){
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
