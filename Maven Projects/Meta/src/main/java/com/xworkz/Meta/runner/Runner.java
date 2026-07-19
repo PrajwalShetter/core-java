391a6b6c24c995eb8c6ae5f9d9e08c88cc2963ff
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-19 23:47:21 +0530

    add transaction concept to the code

--- Maven Projects/Meta/src/main/java/com/xworkz/Meta/runner/Runner.java
+++ Maven Projects/Meta/src/main/java/com/xworkz/Meta/runner/Runner.java
@@ -12,48 +12,55 @@ public class Runner {
 
         SignUpDto user = new SignUpDto();
 
-//        user.setUserName("Prajwal");
-//        user.setAge(23);
-//        user.setMobileNo("9908765432");
-//        user.setPassword("Praj@8769");
-//        user.setConfirmPassword("Praj@8769");
-//        user.setNationality("Indian");
+        user.setUserName("Lakshmana");
+        user.setAge(23);
+        user.setMobileNo("8890878907");
+        user.setPassword("Laku@2134");
+        user.setConfirmPassword("Laku@2134");
+        user.setNationality("Indian");
 
         SignUpController controller = new SignUpController();
+//        controller.register(user);
 
-        //controller.register(user);
+
 
        // controller.getAllData();
 
-        List<SignUpDto> list = controller.retrieveData();
+//        List<SignUpDto> list = controller.retrieveData();
 
-        for (SignUpDto dto : list) {
+//        for (SignUpDto dto : list) {
 
-            System.out.println(dto.getUserName());
-            System.out.println(dto.getAge());
-            System.out.println(dto.getMobileNo());
-            System.out.println(dto.getPassword());
-            System.out.println(dto.getNationality());
-
-            System.out.println("------------------");
-        }
+//            System.out.println(dto.getUserName());
+//            System.out.println(dto.getAge());
+//            System.out.println(dto.getMobileNo());
+//            System.out.println(dto.getPassword());
+//            System.out.println(dto.getNationality());
+//
+//            System.out.println("------------------");
+//        }
 
 //        System.out.println("calling getById in controller");
 //        controller.getById(4);
 
-        System.out.println("Calling getByIdAndName in controller");
-        controller.getByIdAndName(3,"prajwal");
+//        System.out.println("Calling getByIdAndName in controller");
+//        controller.getByIdAndName(3,"prajwal");
+//
+//
+//        System.out.println("calling update() in controller");
+//        controller.update();
+//
+//        System.out.println("calling delete() in controller");
+//        controller.delete();
+//
+//        System.out.println("calling save() in controller");
+//        controller.save(user);
 
-
-        System.out.println("calling update() in controller");
-        controller.update();
-
-        System.out.println("calling delete() in controller");
-        controller.delete();
-
-        System.out.println("calling save() in controller");
-        controller.save(user);
+        System.out.println("Controller calling droptable ");
+        System.out.println(controller.dropTable());
     }
+
+
+
 
 
 
