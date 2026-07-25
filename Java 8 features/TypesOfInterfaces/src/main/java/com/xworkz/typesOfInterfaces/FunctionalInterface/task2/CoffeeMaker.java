f8f2cd5d6ef8a7e3840d3995d047336c11bdf875
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-25 23:46:29 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/CoffeeMaker.java
@@ -0,0 +1,9 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2;
+
+import com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes.Coffee;
+
+@FunctionalInterface
+public interface CoffeeMaker {
+
+    void data(Coffee coffee);
+}
