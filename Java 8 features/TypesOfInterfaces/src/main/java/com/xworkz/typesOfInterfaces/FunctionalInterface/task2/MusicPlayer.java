ee23d14f49ec97f1b083a2ffd9b5649cd3a46cee
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-26 22:53:14 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/MusicPlayer.java
@@ -0,0 +1,9 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2;
+
+import com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes.Song;
+
+@FunctionalInterface
+public interface MusicPlayer {
+
+    void songInfo(Song song);
+}
