1cb12bd692a634050de31290a8f0c48a8049a659
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-27 23:12:19 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Bill.java
@@ -0,0 +1,14 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bill {
+
+    String BillNO;
+    String BillDate;
+}
