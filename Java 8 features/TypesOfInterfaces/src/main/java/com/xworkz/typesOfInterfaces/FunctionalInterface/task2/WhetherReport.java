217e109ee794c45fe13208d312726a3f72a19be6
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-27 23:10:28 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/WhetherReport.java
@@ -0,0 +1,11 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2;
+
+import com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes.Whether;
+
+@FunctionalInterface
+public interface WhetherReport {
+
+     String report(Whether whether);
+
+}
+
