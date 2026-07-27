81f28dc35da208564ffdb0575cee143ee21234cf
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-27 23:12:55 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Drone.java
@@ -0,0 +1,13 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@AllArgsConstructor@NoArgsConstructor
+public class Drone {
+
+    String droneCompany;
+    int speed;
+    String cameraQuality;
+}
