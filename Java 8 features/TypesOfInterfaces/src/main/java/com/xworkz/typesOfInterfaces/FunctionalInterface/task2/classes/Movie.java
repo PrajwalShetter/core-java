92a8694c1386905efb2cf367fa08e79efa476578
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-27 23:13:30 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Movie.java
@@ -0,0 +1,15 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    String name;
+
+
+}
