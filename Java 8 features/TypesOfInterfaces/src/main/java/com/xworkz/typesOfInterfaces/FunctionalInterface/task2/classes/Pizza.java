4fcfccf467eece382a35f4feb84c8fb10fd153e0
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-28 20:23:08 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Pizza.java
@@ -0,0 +1,16 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pizza {
+
+    String pizzaName;
+    double price;
+
+
+}
