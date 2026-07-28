65f5bf7c9e1345927eba05d2a19b3f22720d3986
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-28 20:24:58 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Whether.java
@@ -0,0 +1,14 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Whether {
+
+    String city;
+    double temperature;
+}
