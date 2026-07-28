c178f40ca5d52c8a74ee52dccb0cab4f0bb629da
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-28 20:30:59 +0530

    practice Stream APi concepts on map

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/streamApi/task1/CameraDto.java
@@ -0,0 +1,15 @@
+package com.xworkz.streamApi.task1;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor@AllArgsConstructor
+public class CameraDto {
+
+    private int cameraId;
+    private String brand;
+    double price;
+
+}
