26e80bd63ddd40f3f66391698f65c3bfa8e63b50
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-28 20:24:12 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Song.java
@@ -0,0 +1,14 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor@NoArgsConstructor
+public class Song {
+
+    String name;
+    double duration;
+    String lang;
+}
