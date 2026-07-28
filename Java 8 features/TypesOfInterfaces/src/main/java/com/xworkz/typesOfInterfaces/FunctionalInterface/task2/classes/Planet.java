0821eb13d31f970cc11fa899ac26eef44b3e2bfe
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-28 20:23:24 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Planet.java
@@ -0,0 +1,17 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Planet {
+
+    String name;
+    int gravityPoint;
+    boolean isRain;
+
+}
