8622e1c2afdfbb0ac9d39968c6dbb2dea9a187ef
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-07-28 20:21:44 +0530

    practice programs on Functional interfaces

---
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/task2/classes/Photo.java
@@ -0,0 +1,16 @@
+package com.xworkz.typesOfInterfaces.FunctionalInterface.task2.classes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Photo {
+
+    String px;
+    double size;
+
+}
