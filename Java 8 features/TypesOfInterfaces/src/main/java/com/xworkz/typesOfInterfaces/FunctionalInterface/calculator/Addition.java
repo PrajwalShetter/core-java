85587748a7c2b8b44ce8beb432d0048a5b328ce0
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-08-29 23:50:31 +0530

    changed some logics

--- Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/calculator/Addition.java
+++ Java 8 features/TypesOfInterfaces/src/main/java/com/xworkz/typesOfInterfaces/FunctionalInterface/calculator/Addition.java
@@ -4,4 +4,13 @@ package com.xworkz.typesOfInterfaces.FunctionalInterface.calculator;
 public interface Addition {
 
     void add(int a, int b);
+
+    default int add(int a){
+        int total = 5;
+        return total;
+    }
+
+    static void add(){
+        System.out.println("adding the numbers");
+    }
 }
