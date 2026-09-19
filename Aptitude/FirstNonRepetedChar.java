d24885a5697b40a2b6a5b7b043f468d7be79afc5
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-09-19 22:32:33 +0530

    first not repeated charecter

---
+++ Aptitude/FirstNonRepetedChar.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepetedChar {
+
+    public static void main(String[] args) {
+
+        String word = "swiss";
+
+        Character result = word.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .get();
+
+        System.out.println(result);
+    }
+}
