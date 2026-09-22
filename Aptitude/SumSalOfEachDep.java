0d4d7dec78775563532c4c453379ec15be75836a
prajwal shetter <198955243+PrajwalShetter@users.noreply.github.com>
2026-09-22 23:33:11 +0530

    sum of salary for each departments

---
+++ Aptitude/SumSalOfEachDep.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SumSalOfEachDep {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("Ravi", "IT", 60000),
+                new Employee("Rahul", "IT", 70000),
+                new Employee("Prajwal", "HR", 45000),
+                new Employee("Sneha", "HR", 50000)
+        );
+
+        employees.stream()
+                .collect(Collectors.groupingBy(
+                        emp -> emp.department,
+                        Collectors.summingDouble(emp -> emp.salary)
+                ))
+                .forEach((department, salary) ->
+                        System.out.println(department + " = " + salary));
+    }
+}
